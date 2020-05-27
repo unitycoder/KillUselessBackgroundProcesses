6dc48670345fa9cdd84de12d96523c011597c53e
mika <support@unitycoder.com>
2020-05-27 18:17:35 +0300

    init

---
+++ KillUselessBackgroundProcesses/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillUselessBackgroundProcesses
+{
+    internal static class Extensions
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] uint dwFlags, [Out] StringBuilder lpExeName, [In, Out] ref uint lpdwSize);
+
+        public static string GetMainModuleFileName(this Process process, int buffer = 1024)
+        {
+            var fileNameBuilder = new StringBuilder(buffer);
+            uint bufferLength = (uint)fileNameBuilder.Capacity + 1;
+
+            string ret = null;
+
+            try
+            {
+                //ret = QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? fileNameBuilder.ToString() : null;
+                ret = QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? Path.GetFileName(fileNameBuilder.ToString()) : null;
+            }
+            catch (Exception)
+            {
+                // system process?
+                ret = "(System)";
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ KillUselessBackgroundProcesses/Scanner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillUselessBackgroundProcesses
+{
+    public static class Scanner
+    {
+        // creates list of unwanted processes (based on scanning list)
+        public static void Scan()
+        {
+            // Get all processes running on the local computer.
+            var localProcesses = Process.GetProcesses();
+
+            Array.Sort<Process>(localProcesses, (a, b) => a.ProcessName.CompareTo(b.ProcessName));
+
+            for (int i = 0; i < localProcesses.Length; i++)
+            {
+                Console.WriteLine(localProcesses[i].ProcessName + " " + localProcesses[i].GetMainModuleFileName());
+            }
+
+        }
+    }
+}
