eddd3d96c5826f0b8c2b6124a05b9410294decdb
mika <support@unitycoder.com>
2020-09-17 12:39:12 +0300

    make process scanning with GetIcons work

--- KillUselessBackgroundProcesses/Extensions.cs
+++ KillUselessBackgroundProcesses/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -20,11 +21,13 @@ namespace KillUselessBackgroundProcesses
             uint bufferLength = (uint)fileNameBuilder.Capacity + 1;
 
             string ret = null;
-
             try
             {
+                // access denied
                 //ret = QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? fileNameBuilder.ToString() : null;
-                ret = QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? Path.GetFileName(fileNameBuilder.ToString()) : null;
+                //ret = QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? Path.GetFileName(fileNameBuilder.ToString()) : null;
+                // argument exception
+                return GetExecutablePath(process);
             }
             catch (Exception)
             {
@@ -34,5 +37,132 @@ namespace KillUselessBackgroundProcesses
 
             return ret;
         }
+
+
+        private static string GetExecutablePath(Process Process)
+        {
+            //If running on Vista or later use the new function
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                return GetExecutablePathAboveVista(Process.Id);
+            }
+
+            return Process.MainModule.FileName;
+        }
+
+        // https://stackoverflow.com/a/3654195/5452781 and https://www.giorgi.dev/net-framework/how-to-get-elevated-process-path-in-net/
+        private static string GetExecutablePathAboveVista(int ProcessId)
+        {
+            var buffer = new StringBuilder(1024);
+            IntPtr hprocess = OpenProcess(ProcessAccessFlags.PROCESS_QUERY_LIMITED_INFORMATION, false, ProcessId);
+
+            if (hprocess != IntPtr.Zero)
+            {
+                try
+                {
+                    int size = buffer.Capacity;
+                    if (QueryFullProcessImageName(hprocess, 0, buffer, out size))
+                    {
+                        //Console.WriteLine(buffer.ToString());
+                        return buffer.ToString();
+                    }
+                }
+                finally
+                {
+                    CloseHandle(hprocess);
+                }
+            }
+            //throw new Win32Exception(Marshal.GetLastWin32Error());
+            return null;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern bool QueryFullProcessImageName(IntPtr hprocess, int dwFlags,
+                       StringBuilder lpExeName, out int size);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess,
+                       bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hHandle);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            /// <summary>
+            /// Required to terminate a process using TerminateProcess.
+            /// </summary>
+            Terminate = 1 << 0,
+
+            /// <summary>
+            /// Required to create a thread.
+            /// </summary>
+            CreateThread = 1 << 1,
+
+            /// <summary>
+            /// Required to perform an operation on the address space of a process.
+            /// </summary>
+            VirtualMemoryOperation = 1 << 3,
+
+            /// <summary>
+            /// Required to read memory in a process using ReadProcessMemory.
+            /// </summary>
+            VirtualMemoryRead = 1 << 4,
+
+            /// <summary>
+            /// Required to write to memory in a process using WriteProcessMemory.
+            /// </summary>
+            VirtualMemoryWrite = 1 << 5,
+
+            /// <summary>
+            /// Required to duplicate a handle using DuplicateHandle.
+            /// </summary>
+            DuplicateHandle = 1 << 6,
+
+            /// <summary>
+            /// Required to create a process.
+            /// </summary>
+            CreateProcess = 1 << 7,
+
+            /// <summary>
+            /// Required to set memory limits using SetProcessWorkingSetSize.
+            /// </summary>
+            SetQuota = 1 << 8,
+
+            /// <summary>
+            /// Required to set certain information about a process, such as its priority class.
+            /// </summary>
+            SetInformation = 1 << 9,
+
+            /// <summary>
+            /// Required to retrieve certain information about a process, such as its token, exit code, and priority class.
+            /// </summary>
+            QueryInformation = 1 << 10,
+
+            /// <summary>
+            /// Required to suspend or resume a process.
+            /// </summary>
+            SuspendResume = 1 << 11,
+
+            /// <summary>
+            /// Required to retrieve certain information about a process.
+            /// </summary>
+            QueryLimitedInformation = 1 << 12,
+
+            /// <summary>
+            /// Required to wait for the process to terminate using the wait functions.
+            /// </summary>
+            Synchronize = 1 << 20,
+
+            PROCESS_QUERY_LIMITED_INFORMATION = 0x1000,
+
+            /// <summary>
+            /// All possible access rights for a process object.
+            /// </summary>
+            All = 0x001F0FFF
+        }
+
     }
+
+
 }
--- KillUselessBackgroundProcesses/Scanner.cs
+++ KillUselessBackgroundProcesses/Scanner.cs
@@ -17,6 +17,68 @@ namespace KillUselessBackgroundProcesses
 {
     public static class Scanner
     {
+
+        // https://stackoverflow.com/a/14053737/5452781
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr LoadIcon(IntPtr hInstance, IntPtr lpIconName);
+
+        [DllImport("user32.dll", EntryPoint = "GetClassLong")]
+        static extern uint GetClassLong32(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
+        static extern IntPtr GetClassLong64(IntPtr hWnd, int nIndex);
+
+        /// <summary>
+        /// 64 bit version maybe loses significant 64-bit specific information
+        /// </summary>
+        static IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex)
+        {
+            if (IntPtr.Size == 4)
+                return new IntPtr((long)GetClassLong32(hWnd, nIndex));
+            else
+                return GetClassLong64(hWnd, nIndex);
+        }
+
+        static readonly uint WM_GETICON = 0x007f;
+        static readonly IntPtr ICON_SMALL2 = new IntPtr(2);
+        static readonly IntPtr IDI_APPLICATION = new IntPtr(0x7F00);
+        static readonly int GCL_HICON = -14;
+
+        public static Image GetSmallWindowIcon(IntPtr hWnd)
+        {
+            try
+            {
+                IntPtr hIcon = default(IntPtr);
+
+                hIcon = SendMessage(hWnd, WM_GETICON, ICON_SMALL2, IntPtr.Zero);
+
+                if (hIcon == IntPtr.Zero)
+                    hIcon = GetClassLongPtr(hWnd, GCL_HICON);
+
+                if (hIcon == IntPtr.Zero)
+                    hIcon = LoadIcon(IntPtr.Zero, (IntPtr)0x7F00/*IDI_APPLICATION*/);
+
+                if (hIcon != IntPtr.Zero)
+                    return new Bitmap(Icon.FromHandle(hIcon).ToBitmap(), 16, 16);
+                else
+                    return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
         // creates list of unwanted processes (based on scanning list)
         public static List<ProcessData> Scan()
         {
@@ -29,16 +91,28 @@ namespace KillUselessBackgroundProcesses
 
             for (int i = 0; i < localProcesses.Length; i++)
             {
-                Console.WriteLine(localProcesses[i].ProcessName + " " + localProcesses[i].GetMainModuleFileName());
+                //Console.WriteLine(localProcesses[i].ProcessName + " " + localProcesses[i].GetMainModuleFileName());
 
                 var p = new ProcessData();
                 p.process = localProcesses[i];
 
                 try
                 {
-                    p.Icon = ImageSourceFromBitmap(Icon.ExtractAssociatedIcon(localProcesses[i].MainModule.FileName).ToBitmap());
+                    //Console.WriteLine(p.process);
+                    var procname = p.process.GetMainModuleFileName();
 
+                    Console.WriteLine("name: "+procname);
+                    if (procname != null)
+                    {
+                        p.Icon = ImageSourceFromBitmap(Icon.ExtractAssociatedIcon(procname).ToBitmap());
+                    }
+                    else
+                    {
+                        p.Icon = ImageSourceFromBitmap(Bitmap.FromHicon(SystemIcons.Error.Handle));
 
+                    }
+                    //p.Icon = ImageSourceFromBitmap(Icon.ExtractAssociatedIcon(localProcesses[i].MainModule.FileName).ToBitmap());
+                    //var img = GetSmallWindowIcon(p.process.Handle);//.ToBitmap());
                 }
                 catch (Exception ex)
                 {
@@ -50,9 +124,10 @@ namespace KillUselessBackgroundProcesses
                     else
                     {
                         p.Icon = ImageSourceFromBitmap(Bitmap.FromHicon(SystemIcons.Error.Handle));
-                        throw;
+                        //throw;
                     }
                 }
+
 
                 p.Name = localProcesses[i].ProcessName;
                 p.FileName = localProcesses[i].GetMainModuleFileName();
