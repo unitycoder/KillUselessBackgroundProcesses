0051f4887fb0869f8acc7f31f34165f1c806582b
mika <unitycoder@users.noreply.github.com>
2022-03-24 20:30:06 +0200

    test #BUILD

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "KillUselessBackgroundProcesses\Bin\x64\Release" -ArchiveFileName "KillUselessBackgroundProcessesX64.zip" -Format Zip
+powershell Compress-7Zip "KillUselessBackgroundProcesses\nin\Release\*" -ArchiveFileName "KillUselessBackgroundProcessesX64.zip" -Format Zip
 :exit
 popd
 @echo on
